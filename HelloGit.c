d0d9905bb4f3c3312d0b531b22452f2a76b0cde8
Joseph Espy <jespy@gwu.edu>
2017-09-27 21:32:30 -0400

    Switch from java to C for performance
     - C is far superior to java.  It is more performant and easier to read and write.
     - Runs faster and lower code programmer overhead

---
+++ HelloGit.c
@@ -0,0 +1,24 @@
+/* code for GW ACM Intro to Git + Linux Workshop
+ * authored by Samsara Counts
+ *
+ * Sept. 30, 2017
+ * */
+
+#include "stdio.h"
+
+int samsarac(char * param);
+
+int main() {
+    printf("Welcome to our dummy Java file.\n");
+    samsarac("not suited for l337 h4ck1ng\n");
+
+    /*CALL YOUR FUNCTION HERE */
+    return 0;
+}
+
+
+//dummy function template w/your name
+int samsarac(char * param) {
+    printf("Samsara thinks Java is: %s\n", param);
+    return 0;
+}
